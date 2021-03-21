c31afb81dba8c9b0f7036592ce7a1bf74a617cc2
tkpanke <tpanke16@georgefox.edu>
2021-03-21 10:17:54 -0700

    set up JUnit

---
+++ src/test/java/LongSleepTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+
+public class LongSleepTest {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void secondTest() {
+        // this will obviously fail
+        //Assert.assertTrue(false);
+    }
+}
